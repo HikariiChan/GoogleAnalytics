bed11da24a4b81ca08a4a77bea778a4cbd0deb49
HikariiChan <vern7979@gmail.com>
2022-02-27 00:15:20 +0800

    update web

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,25 @@
 //JS to enable link to tab
 var url = document.location.toString();
 if(url.match('#')){
-  $('.nav-tabs a[href= "#' + url.split('#')[1] + '"]').tab('show');
+  $(newFunction()).tab('show') ;
 }
 
 // Change hash for page-reload
-$('.nav-tabs a').on('shown.bs.tab', function (e) {
+$('.nav-link a').on('shown.bs.tab', function (e) {
   window.location.hash = e.target.hash;
 })
+
+
+function newFunction() {
+  return '.nav-tabs a[href=#' + url.split('#')[1] + ']';
+}
+/* // Javascript to enable link to tab
+var hash = (window.location.href.match(/[?&]tab-name=[^&$]+/i) || '=').split('=')[1];
+if (hash) {
+    $('#nav-list .nav-link a[href="#' + hash + '"]').tab('show');
+} 
+
+// Change hash for page-reload
+$('#nav-list .nav-link a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+}) */
