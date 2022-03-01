832aed313aa492cdcdb03105b9ba489b078bdb8e
HikariiChan <vern7979@gmail.com>
2022-03-01 17:12:15 +0800

    update form

--- js/custom.js
+++ js/custom.js
@@ -78,6 +78,7 @@ function validateForm() {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if(!re.test(email)){
           document.querySelector('.status').innerHTML = "Email format invalid";
+          
           return false;
       }
   }
@@ -95,29 +96,3 @@ function validateForm() {
   
 }
 
-//avoid page reload
-document.getElementById('status').innerHTML = "Sending...";
-formData = {
-'name'     : $('input[name=name]').val(),
-'email'    : $('input[name=email]').val(),
-'subject'  : $('input[name=subject]').val(),
-'message'  : $('textarea[name=message]').val()
-};
-
-
-$.ajax({
-url : "mail.php",
-type: "POST",
-data : formData,
-success: function(data, textStatus, jqXHR)
-{
-
-$('#status').text(data.message);
-if (data.code) //If mail was sent successfully, reset the form.
-$('#contact-form').closest('form').find("input[type=text], textarea").val("");
-},
-error: function (jqXHR, textStatus, errorThrown)
-{
-$('#status').text(jqXHR);
-}
-});
